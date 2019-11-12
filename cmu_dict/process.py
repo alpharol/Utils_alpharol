a65d53264ab5e17c0c506e6c1bf425f41f63903d
alpharol <ascyx1218@163.com>
2019-11-12 15:23:37 +0800

    update files

--- cmu_dict/process.py
+++ cmu_dict/process.py
@@ -2,10 +2,17 @@ from tqdm import trange
 import argparse
 import time
 
-symbols = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2"]
+up = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+low = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+eng_pro = ["0","1","2"]
+symbols = up + low + eng_pro
+
 
 def write_dict():
-    print("read cmu_dict data and write them into the dict")
+    """
+    读入CMU字典
+    """
+    print("------------read cmu_dict data and write them into the dict------------")
     f = open("cmu_dict.txt","r",encoding = "utf-8")
     con = f.readlines()
     f.close()
@@ -16,30 +23,57 @@ def write_dict():
 
     #print(dict["AARTI"])
     print("------------test data--------------")
-    assert dict["AARTI"] == "AA1RTIY2","the dict is not right"
-    print("the dict is right")
+    assert dict["aarti"] == "AA1RTIY2","the dict is not right"
+    #print("the dict is right")
     return dict
 
 def convert_LJ(dict):
-    ###read metadata.txt
-    f = open("LJ_metadata.txt","r")
+    """
+    将LJ-Speech的文本转化为CMU字典。
+    """
+    f = open("LJ_metadata.txt","r",encoding="utf-8")
     meta = f.readlines()
     f.close()
     print("------------converting the metadata to cmu_dict------------")
     for i in trange(len(meta)):
         word = ""
-        tmp_meta = meta[i].split("|")[2].upper().replace(","," , ").replace("."," . ").replace("!"," ! ").replace("?"," ? ").replace("\"","").replace("\'","").replace("\n","").replace(":"," , ").replace(";"," , ")
+        tmp_meta = meta[i].split("|")[2].replace(","," , ").replace("."," . ").replace("!"," ! ").replace("?"," ? ").replace("\n","").replace(":"," , ").replace(";"," , ").replace("(","").replace(")","").replace("[","").replace("]","").replace("-"," ").replace("'s", " 's").replace("'ll", " will").replace("'ve", " have").replace("'m", " am")
         for w in tmp_meta.split(" "):
-            if w in dict.keys():
-                word = word + " " + dict[w]
-            else:
-                word = word + " " + w
-        if word[-2] in [",",".","!","?"]:
-            word = word[:-2] + " % " + word[-2]
-        else:
-            word = word + " % . "
-        meta[i] = bytes(meta[i].split("|")[0]+"|"+word+" \n",encoding = "utf-8")
-
+            t = w.strip()
+            if len(t) != 0:
+                if t in [",",".","!","?"]:
+                    word = word + " " + t
+                else:
+                # 如果只有一个字母的话，大写就是读字母，小写就是按照单词读（a）
+                    if len(t) == 1:
+                        if t == "a" or t == "A":
+                            word = word + " " + dict[t]
+                        else:
+                            word = word + " " + dict[t.upper()]
+                    elif t == "'s":
+                        word = word + " " + dict[t]
+                    else:
+                        # 如果单词不止有一个字母
+                        if t[0] in low:
+                            # 首字母是小写，默认单词的读音
+                            if t.lower() in dict:
+                                word = word + " " + dict[t.lower()]
+                            else:
+                                word = word + " " + t.lower()
+                        elif t[0] in up and t[1] in low:
+                            # 首字母是大写，第二个字母是小写，默认单词读音
+                            if t.lower() in dict:
+                                word = word + " " + dict[t.lower()]
+                            else:
+                                word = word + " " + t.lower()
+                        else:
+                            # 否则就默认是字母的读音
+                            for k in t:
+                                if k == "'":
+                                    pass
+                                else:
+                                    word = word + " " + dict[k.upper()]
+        meta[i] = bytes(meta[i].split("|")[0]+"|"+word+" % "+" \n",encoding = "utf-8")
     f = open("LJ_output.txt","wb")
     f.writelines(meta)
     f.close()
@@ -47,27 +81,52 @@ def convert_LJ(dict):
 
 
 def convert_text(text_list, dict):
-    f = open(text_list,"r")
+    f = open(text_list,"r",encoding = "utf-8")
     meta = f.readlines()
     f.close()
     for i in trange(len(meta)):
-        meta[i] = meta[i].replace(","," , ").replace("."," . ").replace("!"," ! ").replace("?"," ? ").replace("|","| ").replace("\n","")
+        meta[i] = meta[i].replace(","," , ").replace("."," . ").replace("!"," ! ").replace("?"," ? ").replace("|","| ").replace("\n","").replace(":"," , ").replace(";"," , ").replace("(","").replace(")","").replace("[","").replace("]","").replace("-"," ").replace("'s", " 's").replace("'ll", " will").replace("'ve", " have").replace("'m", " am")
         word = ""
         tmp_data = ""
         for n in meta[i].split(" "):
             if len(n.strip()) != 0:
                 if n[-1] in ["1","2","3","4","5","#","%","$",",",".","!","?"]:
+                    # 如果遇到的是中文和符号，不做处理
                     tmp_data = tmp_data + " " + n
                 else:
-                    tmp_data = tmp_data + " " + n.upper()
-        for w in tmp_data.split(" "):
-            if w in dict.keys(): 
-                word = word + " " + dict[w]
-            else:
-                word = word + " " + w
-		if word[-1].strip()[-1] in symbols:
-		    word = word + " " + "."
-        meta[i] = bytes(word + "\n",encoding = "utf-8").lstrip()
+                    # 如果遇到的是英文
+                    if len(n) == 1:
+                        # 如果只有一个字母，主要解决车牌号和“a”
+                        if n == "a" or n == "A":
+                            tmp_data = tmp_data + " " + dict[n]
+                        else:
+                            tmp_data = tmp_data + " " + dict[n.upper()]
+                    elif n == "'s":
+                        tmp_data = tmp_data + " " + dict[n]
+                    else:
+                        # 如果多于一个字母
+                        if n[0] in low:
+                            # 如果首字母是小写，那么默认单词的读音
+                            if n.lower() in dict:
+                                tmp_data = tmp_data + " " + dict[n.lower()]
+                            else:
+                                tmp_data = tmp_data + " " + n.lower()
+                        elif n[0] in up and n[1] in low:
+                            # 如果首字母是大写，第二个字母是小写，默认是单词的读音
+                            if n.lower() in dict:
+                                tmp_data = tmp_data + " " + dict[n.lower()]
+                            else:
+                                tmp_data = tmp_data + " " + n.lower()
+                        else:
+                            # 否则默认字母的读音
+                            for w in n:
+                                if w == "'":
+                                    pass
+                                else:
+                                    tmp_data = tmp_data + " " + dict[w.upper()]
+        if tmp_data[-1].strip()[-1] in symbols:
+            tmp_data = tmp_data + " " + "%"
+        meta[i] = bytes(tmp_data + "\n",encoding = "utf-8").lstrip()
     f = open("output.txt","wb")
     f.writelines(meta)
     f.close()
@@ -75,17 +134,18 @@ def convert_text(text_list, dict):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mode",default="LJ",help="accept mode:LJ for LJspeech; text for normal text")
-	parser.add_argument("--text_list",default="text.txt")
+    parser.add_argument("--mode",default="text",help="accept mode:'LJ' for LJspeech; 'text' for normal text")
+    parser.add_argument("--text_list",default="text.txt")
     args = parser.parse_args()
     
-	text_list = args.text_list
+    text_list = args.text_list
     dict = write_dict()
-	
-    if args.mode == "LJ":
-        convert_LJ(dict)
-    else:
+
+    if args.mode == "text":
         convert_text(text_list, dict)
+    else:
+        convert_LJ(dict)
+
 
 
 if __name__ == "__main__":
